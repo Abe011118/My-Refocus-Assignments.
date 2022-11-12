8683972ed626956b0f5f8d7b46c6ff20c1ba06b0
Abe011118 <xcalibur023@gmail.com>
2022-11-12 15:29:57 +0800

    change function style

--- Home Assignment. Variables & Data Types/bmi_calculator.js
+++ Home Assignment. Variables & Data Types/bmi_calculator.js
@@ -1,9 +1,10 @@
 // <!-- BMI = kg/m2 where kg is a person's weight in kilograms and m2 is their height in metres squared. -->
 
-function BMI(weightInkg, heightIncm){
+const BMI = (weightInkg, heightIncm) => {
     let heightInMeters = heightIncm / 100;
     let sqHt = heightInMeters * heightInMeters;
-    console.log('Your BMI for the height of ' + heightIncm + ' cm. and the weight of ' + weightInkg + ' kg. is ' + weightInkg/sqHt);   
-}
+    console.log('Your BMI for the height of ' + heightIncm + ' cm. and the weight of ' + weightInkg + ' kg. is ' + Math.round(weightInkg/sqHt * 10) / 10); 
+    return  Math.round(weightInkg/sqHt * 10) / 10;
+};
 
-BMI(60, 152);
+BMI(65, 169);
