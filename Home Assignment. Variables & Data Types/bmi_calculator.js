09f5e6ef9cad673aec332a6622f3dc22b0c4d654
Abe011118 <xcalibur023@gmail.com>
2022-11-12 10:21:41 +0800

    Manual Testing for BMI Calculator.

--- Home Assignment. Variables & Data Types/bmi_calculator.js
+++ Home Assignment. Variables & Data Types/bmi_calculator.js
@@ -3,7 +3,21 @@
 function BMI(weightInkg, heightIncm){
     let heightInMeters = heightIncm / 100;
     let sqHt = heightInMeters * heightInMeters;
-    console.log('Your BMI for the height of ' + heightIncm + ' cm. and the weight of ' + weightInkg + ' kg. is ' + weightInkg/sqHt);   
+    console.log('Your BMI for the height of ' + heightIncm + ' cm. and the weight of ' + weightInkg + ' kg. is ' + Math.round(weightInkg/sqHt * 10) / 10); 
+    return  Math.round(weightInkg/sqHt * 10) / 10
 }
 
-BMI(60, 152);
+// BMI(65, 169);
+
+// this is a manual testing
+function test(){
+    const result = BMI(65, 169);
+    const expected = 22.8;
+
+    console.assert(
+        result === expected, 
+            `The result ${result} doesn't match the expected value ${expected}.`
+    )
+}
+
+test();
